986e6b9a98a899873d15145d46c46e28281633bb
Chirag Anand <anand.chirag@gmail.com>
2022-09-21 16:01:50 +0530

    fix crash on Android SDK 31
    Original commit: https://github.com/ionic-team/capacitor-plugins/pull/913

--- share/android/src/main/java/com/capacitorjs/plugins/share/SharePlugin.java
+++ share/android/src/main/java/com/capacitorjs/plugins/share/SharePlugin.java
@@ -109,6 +109,11 @@ public class SharePlugin extends Plugin {
                 intent.putExtra(Intent.EXTRA_SUBJECT, title);
             }
 
+            int flags = PendingIntent.FLAG_UPDATE_CURRENT;
+            if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                flags = flags | PendingIntent.FLAG_MUTABLE;
+            }
+
             Intent chooser = null;
             if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
                 // requestCode parameter is not used. Providing 0
@@ -116,7 +121,7 @@ public class SharePlugin extends Plugin {
                     getContext(),
                     0,
                     new Intent(Intent.EXTRA_CHOSEN_COMPONENT),
-                    PendingIntent.FLAG_UPDATE_CURRENT
+                    flags
                 );
                 chooser = Intent.createChooser(intent, dialogTitle, pi.getIntentSender());
                 chosenComponent = null;
